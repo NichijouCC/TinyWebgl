2d95bdad679fbd94fdc285af29add9c2a83930d9
dongbo <895188445@qq.com>
2019-05-05 21:08:51 +0800

    测试declare global

--- src/Twebgl.ts
+++ src/Twebgl.ts
@@ -2,6 +2,24 @@ import { IcontextOptions, IgeometryInfo, IprogramInfo, Iobject } from "./type";
 import { setGeometry } from "./geometryInfo";
 import { setProgram } from "./programInfo";
 
+declare global {
+    interface WebGLVertexArrayObject extends WebGLObject {}
+
+    interface WebGLRenderingContext {
+        beWebgl2: boolean;
+        bindpoint: number;
+        beActiveVao: boolean;
+        beActiveInstance: boolean;
+        addExtension(extName: string): void;
+        createVertexArray(): any;
+        bindVertexArray(vao?: WebGLVertexArrayObject | null): void;
+        deleteVertexArray(vao: WebGLVertexArrayObject): void;
+
+        vertexAttribDivisor(index: number, divisor: number): void;
+        drawElementsInstanced(mode: number, count: number, type: number, offset: number, instanceCount: number): void;
+        drawArraysInstanced(mode: number, first: number, count: number, instanceCount: number): void;
+    }
+}
 WebGLRenderingContext.prototype.addExtension = function(extname: string): boolean {
     let ext = this.getExtension(extname);
     if (ext) {
